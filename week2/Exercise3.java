80e5b3e0306773d4fb83ecc173a6cd2108082122
AgilEliyev <157578474+AgilEliyev@users.noreply.github.com>
2024-01-31 14:11:29 +0400

    Task3

--- week2/Exercise3.java
+++ week2/Exercise3.java
@@ -1,5 +1,16 @@
 package week2;
 
+import java.util.Scanner;
+
 public class Exercise3 {
-    
+     public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.print("");
+        int seconds = scanner.nextInt();
+        int S=seconds%60;
+        int H=seconds/60;
+        int M= H%60;
+         H= H/60;
+        System.out.print(H+":"+M+":"+S);
+     }
 }
